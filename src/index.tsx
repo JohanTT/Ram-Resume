f38cbb07f01556cca3a43c291a6fc5f6dcfd6865
JohanTT <thejohan39@gmail.com>
2024-04-14 13:11:12 +0700

    Refactor index.tsx to use consistent quotation marks and update theme color in ConfigProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ConfigProvider } from 'antd';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
-    <App />
+    <ConfigProvider theme={{ token: { colorText: "#272822" } }}>
+      <App />
     </ConfigProvider>
   </React.StrictMode>
 );
